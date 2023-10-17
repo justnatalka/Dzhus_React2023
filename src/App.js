6f23979970a4f636dc313cff16c5ee287eba6f83
just.natalka <dzhusnatalka@gmail.com>
2023-10-17 19:52:26 +0300

    hw1-1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+      <div className="App">
+        <div className="Simpsons">
+          <h3>Homer Simpson</h3>
+          <h4>Age: 32</h4>
+            <h4>Loves : Beer</h4>
+            <img
+              src="https://i.pinimg.com/originals/8c/aa/56/8caa567bdbd1190e02ed11553c52626e.jpg"
+              alt="Homer"
+          />
+        </div>
+        <div className="Simpsons">
+          <h3>Lisa Simpson</h3>
+          <h4>Age: 14</h4>
+          <h4>Loves: Science</h4>
+          <img
+              src="https://64.media.tumblr.com/7b6193f0ee05d1b42f512b02614b23b7/248ad68eade476cc-a1/s540x810/08de77b592787eb8317951b10f997fd9dde98f6f.jpg"
+              alt="Lisa"
+          />
+        </div>
+        <div className="Simpsons">
+          <h3>Bart Simpson</h3>
+          <h4>Age: 14</h4>
+          <h4>Loves: To piss teachers off</h4>
+          <img
+              src="https://wallpapers.com/images/featured/bart-simpson-gangster-2x4weva8pc8nlo9i.jpg"
+              alt="Bart"
+          />
+        </div>
+        <div className="Simpsons">
+          <h3>Maggie Simpson</h3>
+          <h4>Age: 2</h4>
+          <h4>Loves: Old teddy bears</h4>
+          <img
+              src="https://i.pinimg.com/736x/f9/88/94/f98894f995ff1f0c50ef142517f20a31.jpg"
+              alt="Maggie"
+          />
+        </div>
+        <div className="Simpsons">
+          <h3>Marge Simpson</h3>
+          <h4>Age: 28</h4>
+          <h4>Loves: Peace </h4>
+          <img
+              src="https://i.pinimg.com/736x/ff/b3/bd/ffb3bdeffb2fde0fee9259cd3a736429.jpg"
+              alt="Marge"
+          />
+        </div>
+      </div>
+  )
 }
 
-export default App;
+export default App
+
+
+// 1. Описати всю сім'ю сімпсонів (5 персонажів).
